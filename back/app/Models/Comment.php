3dd81005b76b0aedf0967aecf646a29e25be5344
kaho-te <kaho.t1031@gmail.com>
2024-03-13 19:25:58 +0900

    プロフィール情報の取得処理を追加

--- back/app/Models/Comment.php
+++ back/app/Models/Comment.php
@@ -9,6 +9,8 @@ class Comment extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['text', 'post_id'];
+
     public function post()
     {
       return $this->belongsTo(Post::class);
--- back/app/Models/Follow.php
+++ back/app/Models/Follow.php
@@ -8,14 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class Follow extends Model
 {
     use HasFactory;
-
-    public function followings()
-    {
-        return $this->belongsToMany(self::class, "follows", "user_id", "following_id")->withTimestamps();
-    }
-
-    public function followers()
-    {
-        return $this->belongsToMany(self::class, "follows", "following_id", "user_id")->withTimestamps();
-    }
 }
