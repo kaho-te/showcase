7f7d6e43e5b3584e8c5a42caf98c063961e23557
kaho-te <kaho.t1031@gmail.com>
2024-03-21 16:05:58 +0900

    ヘッダの修正

--- front/src/src/components/Layouts/AppLayout.js
+++ front/src/src/components/Layouts/AppLayout.js
@@ -7,15 +7,8 @@ const AppLayout = ({ header, children }) => {
     return (
         <div>
             <div className="min-h-screen">
-                {/* Page Heading */}
-                <header className="bg-white">
-                    <div className="max-w-7xl mx-auto pt-6 px-4 sm:px-6 lg:px-8">
-                        {header}
-                    </div>
-                </header>
-
                 {/* Page Content */}
-                <main className="m-4">{children}</main>
+                <main className="">{children}</main>
             </div>
             <footer className="mx-3 flex justify-between items-end">
                 <Footer user={user} />
