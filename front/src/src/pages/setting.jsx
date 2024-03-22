7dcd3f7fe0457c5225435b86cb92e6c631a270e0
kaho-te <kaho.t1031@gmail.com>
2024-03-22 17:28:15 +0900

    settingのリンク修正

--- front/src/src/pages/setting.jsx
+++ front/src/src/pages/setting.jsx
@@ -10,7 +10,7 @@ const setting = () => {
         <>
             <header className="bg-white">
                 <div className="flex justify-center pt-3">
-                    <Link className="absolute left-0 w-6 ml-3" href={user ? `/profile/${user.id}` : '#'}>
+                    <Link className="absolute left-0 w-6 ml-3" href={`/profile/${user.id}`}>
                         <img
                             src="http://localhost/storage/icon/Return.png"
                             alt=""
