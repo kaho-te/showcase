b37c973826edba7805bf75f156446502fade4d4f
root <root@DELL>
2024-03-23 18:04:10 +0900

    view画面の各3Dデータを表示

--- front/src/src/lib/laravelAxios.js
+++ front/src/src/lib/laravelAxios.js
@@ -1,7 +1,7 @@
 import Axios from 'axios'
 
 const laravelAxios = Axios.create({
-    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_URL_LOCAL,
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
     },
--- front/src/src/pages/view/[post_id]/index.jsx
+++ front/src/src/pages/view/[post_id]/index.jsx
@@ -17,18 +17,6 @@ const View = () => {
     const { post_id } = router.query
     const [aspectRatio, setAspectRatio] = useState(1) // アスペクト比の状態を追加
 
-    const Model = () => {
-        const { scene } = useGLTF('/threed/3/uchiwa_fan.glb')
-        // const { scene } = useGLTF(`${process.env.NEXT_PUBLIC_BACKEND_URL}/storage/warehouse/1/threed/01.glb`)
-        return (
-            <primitive
-                object={scene}
-                position={[0, 0, 0]}
-                scale={[0.5, 0.5, 0.5]}
-            />
-        )
-    }
-
     useEffect(() => {
         if (router.isReady && post_id) {
             // post_idが存在し、有効な値を持っていることを確認
@@ -52,86 +40,104 @@ const View = () => {
         setAspectRatio(window.innerWidth / window.innerHeight)
     }, [])
 
+    const Model = () => {
+        // const { scene } = useGLTF('/threed/3/uchiwa_fan.glb')
+        const { scene } = useGLTF(
+            `${process.env.NEXT_PUBLIC_BACKEND_URL}/storage/warehouse/1/threed/${post.warehouse.threed_data}`,
+        )
+        return (
+            <primitive
+                object={scene}
+                position={[0, 0, 0]}
+                scale={[0.5, 0.5, 0.5]}
+            />
+        )
+    }
+
     return (
         <div>
-            <div className="mt-2 mx-3">
-                <Link href="/home">✕</Link>
-            </div>
-            <Canvas
-                style={{
-                    width: '100vw',
-                    height: '60vh',
-                    background: 'white',
-                }}>
-                {/* camera={{ position: [5, 5, 5], near: 0.05 }} */}
-                <group>
-                    <Center>
-                        <Model />
-                    </Center>
-                </group>
-                <PerspectiveCamera
-                    makeDefault
-                    args={[35, aspectRatio, 0.1, 2000]}
-                    position={[2, 2, 2]}
-                    fov={70}
-                />
-                <OrbitControls />
-                <directionalLight position={[-10, 10, 10]} castShadow />
-                <ambientLight intensity={1} />
-                <pointLight position={[5, 5, 5]} />
-            </Canvas>
-            <div className='mx-5'>
-                <div className="my-5 flex">
-                    <div className="flex mr-auto">
-                        <img
-                            className="h-6"
-                            src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/storage/icon/LikeGray.png`}
-                            alt="ハート"
-                        />
-                        <img
-                            className="h-6 mx-2"
-                            src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/storage/icon/CommentGray.png`}
-                            alt="コメント"
-                        />
-                        <img
-                            className="h-6"
-                            src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/storage/icon/MessageGray.png`}
-                            alt="紙飛行機"
-                        />
+            {post ? (
+                <div>
+                    <div className="mt-2 mx-3">
+                        <Link href="/home">✕</Link>
                     </div>
-                    <img
-                        className="h-6"
-                        src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/storage/icon/FavoriteGray.png`}
-                        alt="ブックマーク"
-                    />
-                </div>
-                <div className="flex">
-                    <Link href={post ? `/profile/${post.user.id}` : ''}>
-                        <div className="flex items-center">
-                            <img
-                                className="w-10"
-                                style={{ borderRadius: '50%' }}
-                                src={
-                                    post
-                                        ? `${process.env.NEXT_PUBLIC_BACKEND_URL}/storage/profile/${post.user.account.icon}`
-                                        : ''
-                                }
-                                alt="アイコン"
-                            />
-                            <div className="ml-2 font-bold">
-                                {post ? post.user.name : ''}
+                    <Canvas
+                        style={{
+                            width: '100vw',
+                            height: '60vh',
+                            background: 'white',
+                        }}>
+                        {/* camera={{ position: [5, 5, 5], near: 0.05 }} */}
+                        <group>
+                            <Center>
+                                <Model />
+                            </Center>
+                        </group>
+                        <PerspectiveCamera
+                            makeDefault
+                            args={[35, aspectRatio, 0.1, 2000]}
+                            position={[2, 2, 2]}
+                            fov={70}
+                        />
+                        <OrbitControls />
+                        <directionalLight position={[-10, 10, 10]} castShadow />
+                        <ambientLight intensity={1} />
+                        <pointLight position={[5, 5, 5]} />
+                    </Canvas>
+                    <div className="mx-5">
+                        <div className="my-5 flex">
+                            <div className="flex mr-auto">
+                                <img
+                                    className="h-6"
+                                    src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/storage/icon/LikeGray.png`}
+                                    alt="ハート"
+                                />
+                                <img
+                                    className="h-6 mx-2"
+                                    src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/storage/icon/CommentGray.png`}
+                                    alt="コメント"
+                                />
+                                <img
+                                    className="h-6"
+                                    src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/storage/icon/MessageGray.png`}
+                                    alt="紙飛行機"
+                                />
                             </div>
+                            <img
+                                className="h-6"
+                                src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/storage/icon/FavoriteGray.png`}
+                                alt="ブックマーク"
+                            />
                         </div>
-                    </Link>
+                        <div className="flex">
+                            <Link href={`/profile/${post.user.id}`}>
+                                <div className="flex items-center">
+                                    <img
+                                        className="w-10"
+                                        style={{ borderRadius: '50%' }}
+                                        src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/storage/profile/${post.user.account.icon}`}                                        
+                                        alt="アイコン"
+                                    />
+                                    <div className="ml-2 font-bold">
+                                        {post ? post.user.name : ''}
+                                    </div>
+                                </div>
+                            </Link>
+                        </div>
+                        <div>{post.text}</div>
+                        <p className="flex">
+                            Liked by
+                            <span className="mx-1 font-bold">
+                                Ryotaro ISHII
+                            </span>
+                            and
+                            <span className="ml-1 font-bold">Kaho TERADA</span>
+                        </p>
+                    </div>
                 </div>
-                <div>{post ? post.text : ''}</div>
-                <p className="flex">
-                    Liked by
-                    <span className="mx-1 font-bold">Ryotaro ISHII</span>
-                    and
-                    <span className="ml-1 font-bold">Kaho TERADA</span>
-                </p>
-            </div>
+            ) : (
+                <div></div>
+            )}
         </div>
     )
 }
